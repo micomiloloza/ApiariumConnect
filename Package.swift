b3a4d76df093378047689e522d72682867174b06
Mićo Miloloža <brinaxx@gmail.com>
2023-05-26 21:55:29 +0200

    Deleted old files and generated new project through vapor toolbox

--- Package.swift
+++ Package.swift
@@ -1,34 +1,35 @@
-// swift-tools-version: 5.8
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version:5.8
 import PackageDescription
 
 let package = Package(
     name: "ApiariumConnect",
     platforms: [
-        .macOS(.v12)
-    ],
-    products: [
-        .executable(name: "ApiariumConnect", targets: ["ApiariumConnect"]),
+       .macOS(.v12)
     ],
     dependencies: [
-        .package(
-            url: "https://github.com/vapor/vapor",
-            from: "4.70.0"
-        ),
+        // 💧 A server-side Swift web framework.
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.76.0"),
+        .package(url: "https://github.com/vapor/fluent.git", from: "4.8.0"),
+        .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.7.2"),
     ],
     targets: [
         .executableTarget(
-            name: "ApiariumConnect",
+            name: "App",
             dependencies: [
+                .product(name: "Fluent", package: "fluent"),
+                .product(name: "FluentPostgresDriver", package: "fluent-postgres-driver"),
                 .product(name: "Vapor", package: "vapor")
             ],
-            path: "Sources"
+            swiftSettings: [
+                // Enable better optimizations when building in Release configuration. Despite the use of
+                // the `.unsafeFlags` construct required by SwiftPM, this flag is recommended for Release
+                // builds. See <https://www.swift.org/server/guides/building.html#building-for-production> for details.
+                .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release))
+            ]
         ),
-        .testTarget(
-            name: "ApiariumConnectTests",
-            dependencies: ["ApiariumConnect"],
-            path: "Tests"
-        ),
+        .testTarget(name: "AppTests", dependencies: [
+            .target(name: "App"),
+            .product(name: "XCTVapor", package: "vapor"),
+        ])
     ]
 )
