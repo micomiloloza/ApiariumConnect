dfd69ecf9d1b332f436028453845569318a4d20f
Mićo Miloloža <mico.miloloza26@gmail.com>
2023-05-26 20:22:12 +0200

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ApiariumConnect",
+    platforms: [
+        .macOS(.v12)
+    ],
+    products: [
+        .executable(name: "ApiariumConnect", targets: ["ApiariumConnect"]),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/vapor/vapor",
+            from: "4.70.0"
+        ),
+    ],
+    targets: [
+        .executableTarget(name: "ApiariumConnect", dependencies: [
+            .product(name: "Vapor", package: "vapor")
+        ]),
+        .testTarget(name: "ApiariumConnectTests", dependencies: ["ApiariumConnect"]),
+    ]
+)
