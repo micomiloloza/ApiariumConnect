41c0159705e49731dae166d823fe5d55b9b38ea0
Mićo Miloloža <brinaxx@gmail.com>
2023-06-02 23:04:40 +0200

    - Created WebIndexContext

---
+++ Sources/App/Modules/Web/Templates/Contexts/WebIndexContext.swift
@@ -0,0 +1,14 @@
+//
+//  WebIndexContext.swift
+//  
+//
+//  Created by Mico Miloloza on 31.05.2023..
+//
+
+public struct WebIndexContext {
+    public var title: String
+    
+    public init(title: String) {
+        self.title = title
+    }
+}
